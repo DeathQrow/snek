93d9a49ff1fc2fa716e26ef34a7fd9a2ecc6c143
DeathQrow <48766799+DeathQrow@users.noreply.github.com>
2024-05-11 04:33:00 +0200

    added snake sprites and animations; made second canvas; added dynamic window resizing; swapped from setinterval to  requestAnimationFrame

--- master.js
+++ master.js
@@ -1,16 +1,28 @@
-var fps = document.getElementById("fps");
+var fpscount = document.getElementById("fps");
 var currentFrame = 0;
 var lastFrame = 0;
 var last30fps = [];
 var avgfps = 0;
 var state = 1;
 
+var fps = 60;
+var now;
+var then = Date.now();
+var interval = 1000/fps;
+var delta;
+
 function globalClock(){
+  
+  requestAnimationFrame(globalClock)
+  now = Date.now();
+  delta = now - then;
+  if (delta > interval) {
+    then = now - (delta % interval);
   frameRate()
   soundVol = (soundBar.value/100);
   bgm.volume = (musicBar.value/100);
   if(state==1){
-  if(skipFrame>0&&skipFrame<=(subframes/4)&&canGrace>=1&&graceRequest==1&&document.getElementById("graceToggle").checked==1){
+  if(skipFrame>0&&skipFrame<=(subframes/4)&&canGrace>=1&&graceRequest==1){
     Grace();
     canGrace = 0;
     graceRequest=0;
@@ -21,13 +33,105 @@ function globalClock(){
     skipFrame = 0;
     canGrace += 0.5;
     graceRequest = 0;
-    console.log(canGrace)
   }
   if(Dir.length>2) canGrace = 0;
   skipFrame++;
   }
   Clock();
-  setTimeout(globalClock, 1000/64)
+  }
+}
+
+window.onresize = CanvasResizer;
+
+function CanvasResizer(){
+  if(window.innerHeight*w>(window.innerWidth+130)*h) {
+    size = Math.ceil((window.innerWidth-20)/w);
+  } else {
+    size = Math.ceil((window.innerHeight-130)/h);
+  }
+  if(size%subframes!=0) size-=size%subframes;
+  b.style.background = "repeating-conic-gradient(#3d285d 0% 25%, #432c68 0% 50%) 0% 0% /"+200/w+"% "+200/h+"%";
+  c.width = w*size;
+  c.height = h*size;
+  b.width = w*size;
+  b.height = h*size;
+  if(state==0){
+    for(i=1; i<snake.length-1; i++){
+      if(i>0){
+        if(DirMem[i][0]==1) xSheet=0;
+        else if(DirMem[i][1]==1) xSheet=170;
+        else if(DirMem[i][0]==-1) xSheet=340;
+        else if(DirMem[i][1]==-1) xSheet=510;
+        ctx.drawImage(sheetSnek,xSheet,170,160,160,(snake[i][0])*size, (snake[i][1])*size, size, size);
+      }
+    }
+    for(i=0; i<snake.length; i++){
+      if(DirMem[i]!=DirMem[i+1]&&i<DirMem.length-1&&snake.length>1&&Dir[1]!=DirMem[i+1]){
+        if(i>0){
+          if(DirMem[i][0]==1) {
+            if(DirMem[i+1][1]==1) xSheet=340, Tturn=0;
+            else xSheet=510, Tturn=1;
+          }
+          else if(DirMem[i][1]==1) {
+            if(DirMem[i+1][0]==1) xSheet=0, Tturn=1;
+            else xSheet=510, Tturn=0;
+          }
+          else if(DirMem[i][0]==-1) {
+            if(DirMem[i+1][1]==1) xSheet=170, Tturn=1;
+            else xSheet=0, Tturn=0;
+          }
+          else if(DirMem[i][1]==-1) {
+            if(DirMem[i+1][0]==1) xSheet=170, Tturn=0;
+            else xSheet=340, Tturn=1;
+          }
+        } else {
+          if(Dir[1][0]==1) {
+            if(DirMem[1][1]==1) xSheet=340, Hturn=0, Tturn=0;
+            else xSheet=510, Hturn=1, Tturn=1;
+          }
+          else if(Dir[1][1]==1) {
+            if(DirMem[1][0]==1) xSheet=0, Hturn=1, Tturn=1;
+            else xSheet=510, Hturn=0, Tturn=0;
+          }
+          else if(Dir[1][0]==-1) {
+            if(DirMem[1][1]==1) xSheet=170, Hturn=1, Tturn=1;
+            else xSheet=0, Hturn=0, Tturn=0;
+          }
+          else if(Dir[1][1]==-1) {
+            if(DirMem[1][0]==1) xSheet=170, Hturn=0, Tturn=0;
+            else xSheet=340, Hturn=1, Tturn=1;
+          }
+        }
+        ctx.clearRect(snake[i][0]*size, snake[i][1]*size, size, size);
+        ctx.drawImage(sheetSnek,xSheet,340,160,160,snake[i][0]*size, snake[i][1]*size, size, size);
+      } 
+    }
+    if(DirMem[snake.length-1][0]==1) xSheet=0;
+    else if(DirMem[snake.length-1][1]==1) xSheet=170;
+    else if(DirMem[snake.length-1][0]==-1) xSheet=340;
+    else if(DirMem[snake.length-1][1]==-1) xSheet=510;
+    ctx.drawImage(sheetSnek,xSheet,510,160,160,(snake[snake.length-1][0])*size, (snake[snake.length-1][1])*size, size, size);
+    if(Dir[1][0]==1) xSheet=0;
+    else if(Dir[1][1]==1) xSheet=170;
+    else if(Dir[1][0]==-1) xSheet=340;
+    else if(Dir[1][1]==-1) xSheet=510;
+    ctx.drawImage(sheetSnek,xSheet,0,160,160,(snake[0][0])*size, (snake[0][1])*size, size, size);
+    bctx.fillStyle = "red";
+    for(i=0; i<Apples.length; i++) bctx.fillRect(Apples[i][0]*size,Apples[i][1]*size,size,size);
+    const gradient = bctx.createRadialGradient((snake[0][0]+0.5)*size, (snake[0][1]+0.5)*size, 0.15*size, (snake[0][0]+0.5)*size, (snake[0][1]+0.5)*size,1.5*size);
+    gradient.addColorStop(0, "red");
+    gradient.addColorStop(1, "transparent");
+    bctx.fillStyle = gradient;
+    bctx.fillRect((snake[0][0]-2)*size, (snake[0][1]-2)*size, 5*size, 5*size);
+    ctx.fillStyle = "rgba(50, 50, 50, 80%)";
+    ctx.fillRect(0,0,w*size,h*size);
+    ctx.textAlign = "center";
+    ctx.fillStyle = "darkred";
+    ctx.font = ((w*h)/(w+h)*size/3)+"px 'Patrick Hand'";
+    ctx.fillText("Game Over",(w*size/2),h*size/2.5);
+    ctx.font = ((w*h)/(w+h)*size/6)+"px 'Patrick Hand'";
+    ctx.fillText("Press [R] to restart",w*size/2,h*size/1.8);
+  }
 }
 
 function frameRate(){
@@ -36,7 +140,7 @@ function frameRate(){
   last30fps.push(1000/(currentFrame-lastFrame));
   last30fps.forEach(n => avgfps += n)
   avgfps/= last30fps.length;
-  fps.innerText = (1000/(currentFrame-lastFrame)).toFixed(2)+" fps avg: "+avgfps.toFixed(2);
+  fpscount.innerText = (1000/(currentFrame-lastFrame)).toFixed(2)+" fpscount avg: "+avgfps.toFixed(2);
   lastFrame = time[Symbol.toPrimitive]('number');
 }
 
@@ -100,6 +204,11 @@ window.addEventListener("keydown", function (event) {
       if(state==0){
         preGame()
       }
+      break;
+    case "p":
+      if(fpscount.hidden==1){
+        fpscount.hidden=0;
+      } else fpscount.hidden=1;
       break;
     default:
       return; // Quit when this doesn't handle the key event.
--- snek.js
+++ snek.js
@@ -1,31 +1,35 @@
 //canvas setup
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
+var b = document.getElementById("bgCanvas")
+var bctx = b.getContext("2d")
 var w = 10;
 var h = 10;
-var subframes = 10;
-if(window.innerHeight*w>(window.innerWidth+130)*h) {
-  var size = Math.ceil((window.innerWidth-20)/w);
-} else {
-  var size = Math.ceil((window.innerHeight-130)/h);
-}
-c.style.background = "repeating-conic-gradient(#3d285d 0% 25%, #432c68 0% 50%) 0% 0% /"+200/w+"% "+200/h+"%";
+var subframes = 8;
+var size;
+CanvasResizer();
+var sheetSnek = document.getElementById("snek_sheet");
+var xSheet = 0;
+var ySheet = 0;
+var Hturn = 1;
+var Tturn = 1;
 
-//innitial game set up
+//innitial game set usp
 function preGame(){
-  c.width = w*size;
-  c.height = h*size;
-  ctx.fillStyle = "rgb(50,50,60)";
   ctx.clearRect(0,0,w*size,h*size);
-  Dir = [[1,0],[1,0]];
-  DirMem = [[1,0]];
+  bctx.clearRect(0,0,w*size,h*size);
+  Dir = [[1,0]];
+  Dir[1]=Dir[0];
+  DirMem = [];
+  DirMem[0]=Dir[1];
+  DirMem[1]=Dir[1];
   Score = 0;
   state = 1;
   skipFrame = 0;
   canGrace = 1;
   graceRequest=0;
   document.getElementById("points").innerText = "Score: " + Score;
-  snake = [[Math.floor((w-1)/2),Math.floor((h-1)/2)]];
+  snake = [[Math.floor((w-1)/2),Math.floor((h-1)/2)],[Math.floor((w-1)/2)-1,Math.floor((h-1)/2)]];
   tail = Array.from(snake[snake.length - 1]);
   Apples = [];
   for(var i = 0; i < 3; i++){
@@ -36,29 +40,111 @@ function preGame(){
 function Render(){
   if(state==1) {
     ctx.clearRect(0,0,w*size,h*size)
-    ctx.fillStyle = "red";
-    for(i=0; i<Apples.length; i++) ctx.fillRect(Apples[i][0]*size,Apples[i][1]*size,size,size);
-    
-    for(i=0; i<snake.length; i++){
-      if(DirMem[i-1]!=DirMem[i]&&i<DirMem.length-1&&snake.length>1){
-        ctx.fillStyle = "rgb(120,200,50)";
-        ctx.fillRect((snake[i][0])*size, snake[i][1]*size, size, size);
-      }
+    bctx.clearRect(0,0,w*size,h*size)
+    for(i=1; i<snake.length-1; i++){
       if(i>0){
-        ctx.fillStyle = "rgb(120,200,50)";
-        if(Math.abs(DirMem[i-1][1])!=1){
-          ctx.fillRect((snake[i][0]+(DirMem[i-1][0]*skipFrame/subframes))*size, snake[i][1]*size, size, size);
-        } else if(Math.abs(DirMem[i-1][0])!=1){
-          ctx.fillRect(snake[i][0]*size, (snake[i][1]+(DirMem[i-1][1]*skipFrame/subframes))*size, size, size);
-        }
+        if(DirMem[i][0]==1) xSheet=0;
+        else if(DirMem[i][1]==1) xSheet=170;
+        else if(DirMem[i][0]==-1) xSheet=340;
+        else if(DirMem[i][1]==-1) xSheet=510;
+        ctx.drawImage(sheetSnek,xSheet,170,160,160,(snake[i][0]+(DirMem[i][0]*skipFrame/subframes))*size, (snake[i][1]+(DirMem[i][1]*skipFrame/subframes))*size, size, size);
       }
     }
-    ctx.fillStyle = "rgb(60,150,90)";
-    if(Math.abs(Dir[1][1])!=1){
-      ctx.fillRect((snake[0][0]+(Dir[1][0]*skipFrame/subframes))*size, snake[0][1]*size, size, size);
-    } else if(Math.abs(Dir[1][0])!=1){
-      ctx.fillRect(snake[0][0]*size, (snake[0][1]+(Dir[1][1]*skipFrame/subframes))*size, size, size);
+    for(i=0; i<snake.length; i++){
+      if(DirMem[i]!=DirMem[i+1]&&i<DirMem.length-1&&snake.length>1&&Dir[1]!=DirMem[i+1]){
+        if(i>0){
+          if(DirMem[i][0]==1) {
+            if(DirMem[i+1][1]==1) xSheet=340, Tturn=0;
+            else xSheet=510, Tturn=1;
+          }
+          else if(DirMem[i][1]==1) {
+            if(DirMem[i+1][0]==1) xSheet=0, Tturn=1;
+            else xSheet=510, Tturn=0;
+          }
+          else if(DirMem[i][0]==-1) {
+            if(DirMem[i+1][1]==1) xSheet=170, Tturn=1;
+            else xSheet=0, Tturn=0;
+          }
+          else if(DirMem[i][1]==-1) {
+            if(DirMem[i+1][0]==1) xSheet=170, Tturn=0;
+            else xSheet=340, Tturn=1;
+          }
+        } else {
+          if(Dir[1][0]==1) {
+            if(DirMem[1][1]==1) xSheet=340, Hturn=0, Tturn=0;
+            else xSheet=510, Hturn=1, Tturn=1;
+          }
+          else if(Dir[1][1]==1) {
+            if(DirMem[1][0]==1) xSheet=0, Hturn=1, Tturn=1;
+            else xSheet=510, Hturn=0, Tturn=0;
+          }
+          else if(Dir[1][0]==-1) {
+            if(DirMem[1][1]==1) xSheet=170, Hturn=1, Tturn=1;
+            else xSheet=0, Hturn=0, Tturn=0;
+          }
+          else if(Dir[1][1]==-1) {
+            if(DirMem[1][0]==1) xSheet=170, Hturn=0, Tturn=0;
+            else xSheet=340, Hturn=1, Tturn=1;
+          }
+        }
+        ctx.clearRect(snake[i][0]*size, snake[i][1]*size, size, size);
+        ctx.drawImage(sheetSnek,xSheet,340,160,160,snake[i][0]*size, snake[i][1]*size, size, size);
+      } 
     }
+    if(DirMem[snake.length-1][0]==1) xSheet=0;
+    else if(DirMem[snake.length-1][1]==1) xSheet=170;
+    else if(DirMem[snake.length-1][0]==-1) xSheet=340;
+    else if(DirMem[snake.length-1][1]==-1) xSheet=510;
+    if(DirMem[snake.length-1]!=DirMem[snake.length-2]){
+      if(Tturn==1){
+        ctx.save();
+        ctx.translate((snake[snake.length-1][0]+((1+DirMem[snake.length-1][0]-DirMem[snake.length-1][1])/2))*size,(snake[snake.length-1][1]+((1+DirMem[snake.length-1][0]+DirMem[snake.length-1][1])/2))*size)
+        ctx.rotate(Math.PI*skipFrame/(2*subframes))
+        ctx.clearRect(0,0,(DirMem[snake.length-1][1]-DirMem[snake.length-1][0])*size,(-DirMem[snake.length-1][0]-DirMem[snake.length-1][1])*size);
+        ctx.drawImage(sheetSnek,xSheet,510,160,160,(DirMem[snake.length-1][0]-DirMem[snake.length-1][1]),-(DirMem[snake.length-1][0]-DirMem[snake.length-1][1]), (DirMem[snake.length-1][1]-DirMem[snake.length-1][0])*size, (-DirMem[snake.length-1][0]-DirMem[snake.length-1][1])*size);
+      } else {
+        ctx.save();
+
+        ctx.translate((snake[snake.length-1][0]+((1+DirMem[snake.length-1][0]+DirMem[snake.length-1][1])/2))*size,(snake[snake.length-1][1]+((1-DirMem[snake.length-1][0]+DirMem[snake.length-1][1])/2))*size)
+        ctx.rotate(-Math.PI*skipFrame/(2*subframes))
+        ctx.clearRect(0,0,(-DirMem[snake.length-1][1]-DirMem[snake.length-1][0])*size,(DirMem[snake.length-1][0]-DirMem[snake.length-1][1])*size);
+        ctx.drawImage(sheetSnek,xSheet,510,160,160,(DirMem[snake.length-1][0]-DirMem[snake.length-1][1]),-(DirMem[snake.length-1][0]-DirMem[snake.length-1][1]), (-DirMem[snake.length-1][1]-DirMem[snake.length-1][0])*size, (DirMem[snake.length-1][0]-DirMem[snake.length-1][1])*size);
+      }
+      ctx.restore();
+    }else{
+      ctx.drawImage(sheetSnek,xSheet,510,160,160,(snake[snake.length-1][0]+(DirMem[snake.length-1][0]*skipFrame/subframes))*size, (snake[snake.length-1][1]+(DirMem[snake.length-1][1]*skipFrame/subframes))*size, size, size);
+    }
+    if(Dir[1][0]==1) xSheet=0;
+    else if(Dir[1][1]==1) xSheet=170;
+    else if(Dir[1][0]==-1) xSheet=340;
+    else if(Dir[1][1]==-1) xSheet=510;
+    if(Dir[1]!=DirMem[1]){
+      if(Hturn==1){
+        xSheet-=170;
+        if(xSheet<0) xSheet=510;
+        ctx.save();
+        ctx.translate((snake[0][0]+((1+Dir[1][0]-Dir[1][1])/2))*size,(snake[0][1]+((1+Dir[1][0]+Dir[1][1])/2))*size)
+        ctx.rotate(Math.PI*skipFrame/(2*subframes))
+        if(skipFrame<2) ctx.clearRect((Dir[1][1]-Dir[1][0]),(Dir[1][1]-Dir[1][0]),(Dir[1][1]-Dir[1][0])*size,(-Dir[1][0]-Dir[1][1])*size);
+        ctx.drawImage(sheetSnek,xSheet,0,160,160,0, 0, (Dir[1][1]-Dir[1][0])*size, (-Dir[1][0]-Dir[1][1])*size);
+      } else {
+        xSheet+=170;
+        if(xSheet>510) xSheet=0;
+        ctx.save();
+
+        ctx.translate((snake[0][0]+((1+Dir[1][0]+Dir[1][1])/2))*size,(snake[0][1]+((1-Dir[1][0]+Dir[1][1])/2))*size)
+        ctx.rotate(-Math.PI*skipFrame/(2*subframes))
+        if(skipFrame<2) ctx.clearRect((Dir[1][0]-Dir[1][1]),(Dir[1][0]-Dir[1][1]),(-Dir[1][1]-Dir[1][0])*size,(Dir[1][0]-Dir[1][1])*size);
+        ctx.drawImage(sheetSnek,xSheet,0,160,160,0, 0, (-Dir[1][1]-Dir[1][0])*size, (Dir[1][0]-Dir[1][1])*size);
+      }
+      ctx.restore();
+    }else{
+      ctx.drawImage(sheetSnek,xSheet,0,160,160,(snake[0][0]+(Dir[1][0]*skipFrame/subframes))*size, (snake[0][1]+(Dir[1][1]*skipFrame/subframes))*size, size, size);
+    }
+    bctx.fillStyle = "red";
+    for(i=0; i<Apples.length; i++) bctx.fillRect(Apples[i][0]*size,Apples[i][1]*size,size,size);
+  } else {
+    
   }
 }
 
@@ -80,27 +166,23 @@ function Logic() {
     document.getElementById("points").innerText = "Score: " + Score;
     snake.push(tail);
     DirMem.push(DirMem[DirMem.length-1])
-    ctx.fillStyle = "rgb(120,200,50)";
-    ctx.fillRect(snake[snake.length-1][0]*size, snake[snake.length-1][1]*size, size, size);
     if(Score < (w*h-Apples.length-1)) Apple();
   } 
   if(DirMem.length>=snake.length) {
     DirMem.pop();
   }
-  DirMem.unshift(Dir[1]);
   tail = Array.from(snake[snake.length - 1]);
   if(Dir.length>2) Dir.shift()
   if(Dir.length>4) Dir.splice(4,Infinity)
   Dir[0]=Dir[1];
+  DirMem.unshift(Dir[1]);
   //game over condition
   if((snake[0][0]<0||snake[0][0]>=w||snake[0][1]<0||snake[0][1]>=h||snake.slice(1).find(part => part[0] == snake[0][0] && part[1] == snake[0][1]) != undefined) && state == 1){
-    const gradient = ctx.createRadialGradient((snake[0][0]+0.5)*size, (snake[0][1]+0.5)*size, 0.75*size, (snake[0][0]+0.5)*size, (snake[0][1]+0.5)*size,2*size);
+    const gradient = bctx.createRadialGradient((snake[0][0]+0.5)*size, (snake[0][1]+0.5)*size, 0.15*size, (snake[0][0]+0.5)*size, (snake[0][1]+0.5)*size,1.5*size);
     gradient.addColorStop(0, "red");
     gradient.addColorStop(1, "transparent");
-    ctx.fillStyle = gradient;
-    ctx.fillRect((snake[0][0]-2)*size, (snake[0][1]-2)*size, 5*size, 5*size);
-    ctx.fillStyle = "rgb(60,150,90)";
-    ctx.fillRect(snake[0][0]*size, snake[0][1]*size, size, size);
+    bctx.fillStyle = gradient;
+    bctx.fillRect((snake[0][0]-2)*size, (snake[0][1]-2)*size, 5*size, 5*size);
     ctx.fillStyle = "rgba(50, 50, 50, 80%)";
     ctx.fillRect(0,0,w*size,h*size);
     ctx.textAlign = "center";
@@ -129,7 +211,10 @@ function Logic() {
 }
 
 function Grace(){
-  if(Dir.length>2&&Dir[0][0]!=Dir[2][0]*-1&&Dir[0][1]!=Dir[2][1]*-1) Dir.shift()
+  if(Dir.length>2&&Dir[0][0]!=Dir[2][0]*-1&&Dir[0][1]!=Dir[2][1]*-1) {
+    Dir.shift();
+    DirMem[0]=Dir[1];
+  }
 }
 
 function Apple(){
@@ -138,6 +223,6 @@ function Apple(){
     var yRand = Math.floor(Math.random() * h);
   } while(snake.find(part => part[0] == xRand && part[1] == yRand) != undefined || (Apples.find(ap => ap[0] == xRand && ap[1] == yRand) != undefined))
   Apples.push([xRand, yRand]);
-  ctx.fillStyle = "red";
-  ctx.fillRect(xRand*size,yRand*size,size,size);
+  bctx.fillStyle = "red";
+  bctx.fillRect(xRand*size,yRand*size,size,size);
 }
